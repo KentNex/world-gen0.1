28b75c5d019135ec42a2e9cdd069aa24d8d81bdf
KentNex <33816996+KentNex@users.noreply.github.com>
2017-11-22 15:17:01 -0800

    Sand generation added
    Fixed some bugs along with rehauling the island generation and adding beaches

--- main.lua
+++ main.lua
@@ -5,10 +5,10 @@ function love.load()
   math.randomseed(os.time())
   
   xMap = map()
-  xMap:load(400, {1920,1080}, {30,30})
+  xMap:load(400, {1920,1080}, {80,80})
   xMap:set_map()
   
-  xMap:spawn_island(30)
+  xMap:spawn_island(4000, 40, 40, 0.8, 0.4) --int Island Size, Int X, Int Y, Int island variance(0 - 0.99), Int sand percent(0 - 0.99)
   
   test = 0
   test1 = 0
--- map.lua
+++ map.lua
@@ -13,6 +13,7 @@ function map()
     
     waterAnim = new_animation(love.graphics.newImage("img/watersheet.png"), 400, 400)
     grassAnim = new_animation(love.graphics.newImage("img/grass.png"), 400, 400)
+    sandAnim = new_animation(love.graphics.newImage("img/sand.png"), 400, 400)
     
     for i=0,mapSize[2]-1 do
       obj.map[i] = {}
@@ -21,6 +22,8 @@ function map()
         obj.map[i][n]["type"] = ""
         obj.map[i][n]["x"] = n*(screenSize[1]/mapSize[1])
         obj.map[i][n]["y"] = i*(screenSize[2]/mapSize[2])
+        obj.map[i][n]["xNum"] = n
+        obj.map[i][n]["yNum"] = i
         obj.map[i][n]["xRatio"] = screenSize[1]/((mapSize[1])*tileSize)
         obj.map[i][n]["yRatio"] = screenSize[2]/((mapSize[2])*tileSize)
         obj.map[i][n]["animation"] = {}
@@ -31,75 +34,92 @@ function map()
   function obj:set_map() -- test function to set all to one type
     for i=0, mapSize[2]-1 do
       for n=0, mapSize[1]-1 do
-        obj.map[i][n]["type"] = "water"
-        obj.map[i][n]["animation"]["anim"] = waterAnim
-        obj.map[i][n]["animation"]["dur"] = 10
-        obj.map[i][n]["animation"]["curTime"] = math.random(10)
-        --end
+        obj:tile_water(obj.map[i][n])
       end
     end
   end
   
-  function obj:spawn_island(sz)
-    local centerx, centery, islandmap, size
+  function obj:spawn_island(sz, x, y, vari, sand) -- int Size, int X location, int Y location
+  
+    local islandmap = {}
+    local size = sz
+    local variation = vari
+    local sandPerc = sand * sz
     
-    islandmap = {}
-    size = sz - 1
-    
-    centerx = mapSize[1]/2
-    centery = mapSize[2]/2
-    
-    islandmap[1] = {centerx, centery}
-    
-    obj.map[islandmap[1][1]][islandmap[1][2]]["type"] = "grass"
-    obj.map[islandmap[1][1]][islandmap[1][2]]["animation"]["anim"] = grassAnim
-    obj.map[islandmap[1][1]][islandmap[1][2]]["animation"]["dur"] = 10
-    obj.map[islandmap[1][1]][islandmap[1][2]]["animation"]["curTime"] = math.random(10)
-    
-    table.insert(islandmap, {islandmap[1][1]-1, islandmap[1][2]})
-    table.insert(islandmap, {islandmap[1][1], islandmap[1][2]+1})
-    table.insert(islandmap, {islandmap[1][1]+1, islandmap[1][2]})
-    table.insert(islandmap, {islandmap[1][1], islandmap[1][2]-1})
+    islandmap[1] = obj.map[x][y]
     
     while size > 0 do
       
-      if obj.map[islandmap[1][1]][islandmap[1][2]]["type"] ~= "grass" and math.random() > 0.90 then
-        
-        obj.map[islandmap[1][1]][islandmap[1][2]]["type"] = "grass"
-        obj.map[islandmap[1][1]][islandmap[1][2]]["animation"]["anim"] = grassAnim
-        obj.map[islandmap[1][1]][islandmap[1][2]]["animation"]["dur"] = 10
-        obj.map[islandmap[1][1]][islandmap[1][2]]["animation"]["curTime"] = math.random(10)
-        
-        table.insert(islandmap, {islandmap[1][1]-1, islandmap[1][2]})
-        table.insert(islandmap, {islandmap[1][1], islandmap[1][2]+1})
-        table.insert(islandmap, {islandmap[1][1]+1, islandmap[1][2]})
-        table.insert(islandmap, {islandmap[1][1], islandmap[1][2]-1})
-        
+      if math.random() < variation then
+        table.insert(islandmap, islandmap[1])
         table.remove(islandmap, 1)
         
-        size = size - 1
+      elseif islandmap[1]["type"] == "water" then
         
-      elseif obj.map[islandmap[1][1]][islandmap[1][2]]["type"] == "grass" then
+        if size > sandPerc then
+          obj:tile_grass(islandmap[1])
+        else
+          obj:tile_sand(islandmap[1])
+        end
         
+        table.insert(islandmap, obj:up(islandmap[1]))
+        table.insert(islandmap, obj:down(islandmap[1]))
+        table.insert(islandmap, obj:left(islandmap[1]))
+        table.insert(islandmap, obj:right(islandmap[1]))
         table.remove(islandmap, 1)
-        
+        size = size -1
       else
-        table.insert(islandmap, {islandmap[1][1], islandmap[1][2]})
         table.remove(islandmap, 1)
       end
     end
-    
+  end
+  
+  function obj:up(mp) -- send a map[y][x] and it returns the map[y-1][x]
+    local y
+    if mp["yNum"]-1 < 0 then
+      y = mp["yNum"]
+    else 
+      y = mp["yNum"]-1 
+    end
+    return obj.map[y][mp["xNum"]]
+  end
+  
+  function obj:down(mp) -- send a map[y][x] and it returns the map[y+1][x]
+    local y
+    if mp["yNum"]+1 >= mapSize[2] then
+      y = mp["yNum"]
+    else 
+      y = mp["yNum"]+1 
+    end
+    return obj.map[y][mp["xNum"]]
+  end
+  
+  function obj:left(mp) -- send a map[y][x] and it returns the map[y][x-1]
+    local x
+    if mp["xNum"]-1 < 0 then
+      x = mp["xNum"]
+    else 
+      x = mp["xNum"]-1 
+    end
+    return obj.map[mp["yNum"]][x]
+  end
+  
+  function obj:right(mp) -- send a map[y][x] and it returns the map[y][x+1]
+    local x
+    if mp["xNum"]+1 >= mapSize[1] then
+      x = mp["xNum"]
+    else 
+      x = mp["xNum"]+1 
+    end
+    return obj.map[mp["yNum"]][x]
   end
   
   function obj:draw() -- draws either images or animations
     for i=0, mapSize[2]-1 do
       for n=0, mapSize[1]-1 do
-        if obj.map[i][n]["animation"] == nil then
-          love.graphics.draw(obj.map[i][n]["image"], obj.map[i][n]["x"], obj.map[i][n]["y"], 0, obj.map[i][n]["xRatio"], obj.map[i][n]["yRatio"])
-        else
           local spriteNum = math.floor(obj.map[i][n]["animation"]["curTime"] / obj.map[i][n]["animation"]["dur"] * #obj.map[i][n]["animation"]["anim"].quads) + 1
+          
           love.graphics.draw(obj.map[i][n]["animation"]["anim"].spriteSheet, obj.map[i][n]["animation"]["anim"].quads[spriteNum], obj.map[i][n]["x"], obj.map[i][n]["y"], 0, obj.map[i][n]["xRatio"], obj.map[i][n]["yRatio"])
-        end
       end
     end
   end
@@ -107,14 +127,33 @@ function map()
   function obj:map_update(dt) -- updates the animations
     for i=0, mapSize[2]-1 do
       for n=0, mapSize[1]-1 do
-        if obj.map[i][n]["image"] == nil then
-          obj.map[i][n]["animation"]["curTime"] = obj.map[i][n]["animation"]["curTime"] + dt
-          if obj.map[i][n]["animation"]["curTime"] >= obj.map[i][n]["animation"]["dur"] then
-            obj.map[i][n]["animation"]["curTime"] = obj.map[i][n]["animation"]["curTime"] - obj.map[i][n]["animation"]["dur"]
-          end
+        obj.map[i][n]["animation"]["curTime"] = obj.map[i][n]["animation"]["curTime"] + dt
+        if obj.map[i][n]["animation"]["curTime"] >= obj.map[i][n]["animation"]["dur"] then
+          obj.map[i][n]["animation"]["curTime"] = obj.map[i][n]["animation"]["curTime"] - obj.map[i][n]["animation"]["dur"]
         end
       end
     end
+  end
+  
+  function obj:tile_grass(map_tile) -- sets the tile to a grass tile
+    map_tile["type"] = "grass"
+    map_tile["animation"]["anim"] = grassAnim
+    map_tile["animation"]["dur"] = 10
+    map_tile["animation"]["curTime"] = math.random(10)
+  end
+  
+  function obj:tile_water(map_tile) -- sets the tile to a water tile
+    map_tile["type"] = "water"
+    map_tile["animation"]["anim"] = waterAnim
+    map_tile["animation"]["dur"] = 3
+    map_tile["animation"]["curTime"] = math.random(3)
+  end
+  
+  function obj:tile_sand(map_tile)  -- sets the tile to a sand tile
+    map_tile["type"] = "sand"
+    map_tile["animation"]["anim"] = sandAnim
+    map_tile["animation"]["dur"] = 10
+    map_tile["animation"]["curTime"] = math.random(10)
   end
   
   function new_animation(image, width, height, duration) -- SpriteSheet, width and height of spritesheet and duration of the whole animation(ie: two frams with a duration of 10 is every 5 seconds)
