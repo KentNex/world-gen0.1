3ededf7f346daeb386100534dbbf212aaa01f9d4
KentNex <33816996+KentNex@users.noreply.github.com>
2017-11-22 12:06:50 -0800

    Adding island gen
    Added island generation

--- main.lua
+++ main.lua
@@ -1,10 +1,17 @@
 require("map")
 
 function love.load()
-
+  
+  math.randomseed(os.time())
+  
   xMap = map()
-  xMap:load(400, {1920,1080}, {10,10})
-  xMap:resetMap("water", "img/water.png")
+  xMap:load(400, {1920,1080}, {30,30})
+  xMap:set_map()
+  
+  xMap:spawn_island(30)
+  
+  test = 0
+  test1 = 0
   
   -- Inits
   love.window.setTitle("Map Generator")
@@ -12,8 +19,15 @@ function love.load()
   --graphics
 end
 
+function love.update(dt)
+  xMap:map_update(dt)
+  
+  test = 0--xMap.map[0][0]["anim"].duration--test + dt
+  test1 = 0--xMap.map[0][0]["anim"].currentTime --+ test1
+end
+
 function love.draw()  
-  xMap.draw()
+  xMap:draw()
   --DEBUG TEXT
-  love.graphics.print(xMap.map[0][0]["x"], 0, 0)
+  love.graphics.print(test .. " " .. test1, 0, 0)
 end
--- map.lua
+++ map.lua
@@ -2,12 +2,17 @@ function map()
   local obj = {} -- Creating the object to return
   local tileSize, screenSize, mapSize -- Local varaibles
   
+  local waterAnim -- Animations
+  
   obj.map = {}
   
-  function obj:load(tS, sS, mS)
+  function obj:load(tS, sS, mS) -- int tileSize, int screenSize(x,y), int mapSize(x,y)
     tileSize = tS -- This is the size of the image files (should be around 100)
     screenSize = {sS[1], sS[2]} -- This is the size of the screen
     mapSize = {mS[1], mS[2]} -- This is the size of time map x and y, 1 = tile
+    
+    waterAnim = new_animation(love.graphics.newImage("img/watersheet.png"), 400, 400)
+    grassAnim = new_animation(love.graphics.newImage("img/grass.png"), 400, 400)
     
     for i=0,mapSize[2]-1 do
       obj.map[i] = {}
@@ -18,42 +23,111 @@ function map()
         obj.map[i][n]["y"] = i*(screenSize[2]/mapSize[2])
         obj.map[i][n]["xRatio"] = screenSize[1]/((mapSize[1])*tileSize)
         obj.map[i][n]["yRatio"] = screenSize[2]/((mapSize[2])*tileSize)
-        obj.map[i][n]["image"] = nil
+        obj.map[i][n]["animation"] = {}
         end
     end
   end
   
-  function obj:resetMap(typ, img)
+  function obj:set_map() -- test function to set all to one type
     for i=0, mapSize[2]-1 do
       for n=0, mapSize[1]-1 do
-        obj.map[i][n]["type"] = typ
-        obj.map[i][n]["image"] = love.graphics.newImage(img)
+        obj.map[i][n]["type"] = "water"
+        obj.map[i][n]["animation"]["anim"] = waterAnim
+        obj.map[i][n]["animation"]["dur"] = 10
+        obj.map[i][n]["animation"]["curTime"] = math.random(10)
+        --end
       end
     end
   end
   
-  function obj:draw()
+  function obj:spawn_island(sz)
+    local centerx, centery, islandmap, size
+    
+    islandmap = {}
+    size = sz - 1
+    
+    centerx = mapSize[1]/2
+    centery = mapSize[2]/2
+    
+    islandmap[1] = {centerx, centery}
+    
+    obj.map[islandmap[1][1]][islandmap[1][2]]["type"] = "grass"
+    obj.map[islandmap[1][1]][islandmap[1][2]]["animation"]["anim"] = grassAnim
+    obj.map[islandmap[1][1]][islandmap[1][2]]["animation"]["dur"] = 10
+    obj.map[islandmap[1][1]][islandmap[1][2]]["animation"]["curTime"] = math.random(10)
+    
+    table.insert(islandmap, {islandmap[1][1]-1, islandmap[1][2]})
+    table.insert(islandmap, {islandmap[1][1], islandmap[1][2]+1})
+    table.insert(islandmap, {islandmap[1][1]+1, islandmap[1][2]})
+    table.insert(islandmap, {islandmap[1][1], islandmap[1][2]-1})
+    
+    while size > 0 do
+      
+      if obj.map[islandmap[1][1]][islandmap[1][2]]["type"] ~= "grass" and math.random() > 0.90 then
+        
+        obj.map[islandmap[1][1]][islandmap[1][2]]["type"] = "grass"
+        obj.map[islandmap[1][1]][islandmap[1][2]]["animation"]["anim"] = grassAnim
+        obj.map[islandmap[1][1]][islandmap[1][2]]["animation"]["dur"] = 10
+        obj.map[islandmap[1][1]][islandmap[1][2]]["animation"]["curTime"] = math.random(10)
+        
+        table.insert(islandmap, {islandmap[1][1]-1, islandmap[1][2]})
+        table.insert(islandmap, {islandmap[1][1], islandmap[1][2]+1})
+        table.insert(islandmap, {islandmap[1][1]+1, islandmap[1][2]})
+        table.insert(islandmap, {islandmap[1][1], islandmap[1][2]-1})
+        
+        table.remove(islandmap, 1)
+        
+        size = size - 1
+        
+      elseif obj.map[islandmap[1][1]][islandmap[1][2]]["type"] == "grass" then
+        
+        table.remove(islandmap, 1)
+        
+      else
+        table.insert(islandmap, {islandmap[1][1], islandmap[1][2]})
+        table.remove(islandmap, 1)
+      end
+    end
+    
+  end
+  
+  function obj:draw() -- draws either images or animations
     for i=0, mapSize[2]-1 do
       for n=0, mapSize[1]-1 do
-        love.graphics.draw(obj.map[i][n]["image"], obj.map[i][n]["x"],obj.map[i][n]["y"],0,obj.map[i][n]["xRatio"],obj.map[i][n]["yRatio"])
+        if obj.map[i][n]["animation"] == nil then
+          love.graphics.draw(obj.map[i][n]["image"], obj.map[i][n]["x"], obj.map[i][n]["y"], 0, obj.map[i][n]["xRatio"], obj.map[i][n]["yRatio"])
+        else
+          local spriteNum = math.floor(obj.map[i][n]["animation"]["curTime"] / obj.map[i][n]["animation"]["dur"] * #obj.map[i][n]["animation"]["anim"].quads) + 1
+          love.graphics.draw(obj.map[i][n]["animation"]["anim"].spriteSheet, obj.map[i][n]["animation"]["anim"].quads[spriteNum], obj.map[i][n]["x"], obj.map[i][n]["y"], 0, obj.map[i][n]["xRatio"], obj.map[i][n]["yRatio"])
+        end
       end
     end
   end
-
-  function obj:up(mp)
-    
+  
+  function obj:map_update(dt) -- updates the animations
+    for i=0, mapSize[2]-1 do
+      for n=0, mapSize[1]-1 do
+        if obj.map[i][n]["image"] == nil then
+          obj.map[i][n]["animation"]["curTime"] = obj.map[i][n]["animation"]["curTime"] + dt
+          if obj.map[i][n]["animation"]["curTime"] >= obj.map[i][n]["animation"]["dur"] then
+            obj.map[i][n]["animation"]["curTime"] = obj.map[i][n]["animation"]["curTime"] - obj.map[i][n]["animation"]["dur"]
+          end
+        end
+      end
+    end
   end
   
-  function obj:down(mp)
+  function new_animation(image, width, height, duration) -- SpriteSheet, width and height of spritesheet and duration of the whole animation(ie: two frams with a duration of 10 is every 5 seconds)
+    local animation = {}
+    animation.spriteSheet = image;
+    animation.quads = {}
+    for x = 0, image:getHeight() - height, height do
+      for n = 0, image:getWidth() - width, width do
+        table.insert(animation.quads, love.graphics.newQuad(n,x,width,height, image:getDimensions()))
+      end
+    end
     
-  end
-  
-  function obj:left(mp)
-    
-  end
-  
-  function obj:right(mp)
-    
+    return animation
   end
   
   return obj
